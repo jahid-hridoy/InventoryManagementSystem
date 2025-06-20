9643add04ee73fbee05ceb537ff2bb1b13f8068b
jahid-hridoy <jahidulislam453201@gmail.com>
2025-06-20 17:32:30 +0600

    Added Database and run a migration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using InventoryProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<InventoryDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
